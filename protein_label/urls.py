2db88e5bb27b536dabce0639e20e8a697f04c416
shivamaniarunachalam <bhuniasm@gmail.com>
2024-11-05 21:58:50 -0500

    Changes made at views.py for export options

--- protein_label/urls.py
+++ protein_label/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('upload/', views.process_excel, name='process_excel'),
+    path('', views.process_excel, name='process_excel'),
 path('download/csv/', views.download_csv, name='download_csv'),
 path('download/pdf/', views.download_pdf, name='download_pdf'),
 ]
