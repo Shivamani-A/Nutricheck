4dd012c666bab155ef03962f898a224c77ea4d7b
shivamaniarunachalam <bhuniasm@gmail.com>
2024-11-20 23:20:03 -0500

    added charts

--- protein_label/urls.py
+++ protein_label/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
 path('', views.home, name='home'),
     path('upload/', views.process_excel, name='process_excel'),
-path('download/csv/', views.download_csv, name='download_csv'),
-path('download/pdf/', views.download_pdf, name='download_pdf'),
-path('chart/', views.chart_view, name='chart'),
+    path('download/csv/', views.download_csv, name='download_csv'),
+    path('download/pdf/', views.download_pdf, name='download_pdf'),
+    path('chart/', views.chart_view, name='chart'),
 ]
